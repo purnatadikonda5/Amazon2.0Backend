e56bb94cc1ed51e5cf531e275e5890e37a436686
purnatadikonda5 <purnatadikonda5@gmail.com>
2026-04-05 19:59:09 +0530

    Created basic setup

---
+++ src/main/java/com/purna/Amazon2dotOBackendApplication.java
@@ -0,0 +1,13 @@
+package com.purna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Amazon2dotOBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Amazon2dotOBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/purna/Amazon2dotOBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.purna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Amazon2dotOBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
